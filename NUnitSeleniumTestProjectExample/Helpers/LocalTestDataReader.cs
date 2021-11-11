265ced6661a71053197973de4ea0d7515c7240de
Viacheslav Levkoniuk <v.levkoniuk@gmail.com>
2021-11-11 13:42:00 +0200

    Added reading of the Test Data file to the test case setup
    * added private var to TestData folder with the data json files
    * refactored existing data reader static methods
    * read sample data file in the SetUp section

--- NUnitSeleniumTestProjectExample/Helpers/LocalTestDataReader.cs
+++ NUnitSeleniumTestProjectExample/Helpers/LocalTestDataReader.cs
@@ -7,10 +7,15 @@ namespace NUnitSeleniumTestProjectExample.Helpers
 {
     public static class LocalTestDataReader
     {
+        private static string path = Path.Combine(Directory.GetParent(
+            Directory.GetCurrentDirectory()).Parent.Parent.FullName, 
+            "Models", "TestData");
         public static List<T> LoadTestDataList<T>(string filename)
         {
+            filename = filename + ".json";
+            string path2file = Path.Combine(path, filename);
             List<T> retList = new List<T>();
-            using (StreamReader r = new StreamReader(filename))
+            using (StreamReader r = new StreamReader(path2file))
             {
                 string json = r.ReadToEnd();
                 List<T> items = JsonSerializer.Deserialize<List<T>>(json);
@@ -21,7 +26,9 @@ namespace NUnitSeleniumTestProjectExample.Helpers
 
         public static T LoadTestData<T>(string filename)
         {
-            using (StreamReader r = new StreamReader(filename))
+            filename = filename + ".json";
+            string path2file = Path.Combine(path, filename);
+            using (StreamReader r = new StreamReader(path2file))
             {
                 string json = r.ReadToEnd();
                 T item = JsonSerializer.Deserialize<T>(json);
--- NUnitSeleniumTestProjectExample/Tests/TesteExample.cs
+++ NUnitSeleniumTestProjectExample/Tests/TesteExample.cs
@@ -1,20 +1,27 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using NUnitSeleniumTestProjectExample.Helpers;
+using NUnitSeleniumTestProjectExample.Models;
 using NUnitSeleniumTestProjectExample.PageObjects;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using SeleniumExtras.PageObjects;
+using System.Reflection;
 
 namespace NUnitSeleniumTestProjectExample.Tests
 {
     public class TesteExample
     {
         private IWebDriver driver;
+        private Login _loginInfo;
         [SetUp]
         public void Setup()
         {
             driver = new ChromeDriver();
+            _loginInfo = new Login();
+            _loginInfo = LocalTestDataReader.LoadTestData<Login>(_loginInfo.GetType().Name);
+
         }
 
         [Test]
